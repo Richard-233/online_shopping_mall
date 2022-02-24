9b2285497db15d173ab22b7aa9d0cefe847bcc99
Richard Young <565435857@qq.com>
2022-02-24 22:36:45 +0800

    springboot-mybatis基础框架

--- src/main/java/com/team07/online_shopping_mall/mapper/ShopMapper.java
+++ src/main/java/com/team07/online_shopping_mall/mapper/ShopMapper.java
@@ -2,6 +2,8 @@ package com.team07.online_shopping_mall.mapper;
 
 import com.team07.online_shopping_mall.model.domain.Shop;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 /**
  * <p>
@@ -11,6 +13,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @author team07
  * @since 2022-02-24
  */
+@Repository
 public interface ShopMapper extends BaseMapper<Shop> {
 
 }
--- src/main/java/com/team07/online_shopping_mall/mapper/UserMapper.java
+++ src/main/java/com/team07/online_shopping_mall/mapper/UserMapper.java
@@ -2,6 +2,8 @@ package com.team07.online_shopping_mall.mapper;
 
 import com.team07.online_shopping_mall.model.domain.User;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.stereotype.Repository;
 
 /**
  * <p>
@@ -11,6 +13,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @author team07
  * @since 2022-02-24
  */
+@Repository
 public interface UserMapper extends BaseMapper<User> {
 
 }
