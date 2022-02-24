42a8df42c3a1b8cb97f3a39892d73fbccbf8f24d
Richard Young <565435857@qq.com>
2022-02-24 16:00:56 +0800

    springboot-mybatis基础框架

--- src/main/java/com/team07/online_shopping_mall/MybatisplusApplication.java
+++ src/main/java/com/team07/online_shopping_mall/MybatisplusApplication.java
@@ -4,8 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author team07
+ */
 @SpringBootApplication
-@MapperScan({"com.example.mybatisplus.mapper"})
+@MapperScan({"com.team07.online_shopping_mall.mapper"})
 public class MybatisplusApplication {
 
     public static void main(String[] args) {
