2a102a6dee5045eccbf6274b655e4ab652f69951
Richard Young <565435857@qq.com>
2022-02-25 23:39:20 +0800

    springboot-mybatis基础框架

---
+++ src/main/java/com/team07/online_shopping_mall/common/utls/SecurityUtils.java
@@ -0,0 +1,46 @@
+package com.team07.online_shopping_mall.common.utls;
+
+import com.team07.online_shopping_mall.model.domain.User;
+import com.team07.online_shopping_mall.model.dto.UserInfoDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class SecurityUtils {
+    /**
+     * 获取当前用户
+     *
+     * @return
+     */
+    public static User getCurrentUserInfo() {
+        User userInfo = SessionUtils.getCurrentUserInfo();
+        //模拟登录
+        if (userInfo == null) {
+            userInfo = new User();
+            userInfo.setUsername("模拟");
+        }
+
+        return userInfo;
+    }
+
+    public static UserInfoDTO getUserInfo() {
+        User userInfo = SessionUtils.getCurrentUserInfo();
+        UserInfoDTO userInfoDTO = new UserInfoDTO();
+        //模拟登录
+        if (userInfo == null) {
+            userInfo = new User();
+            userInfo.setUsername("模拟用户");
+            userInfoDTO.setId(1L);
+            userInfoDTO.setName("模拟用户");
+            userInfoDTO.setUserType(1L);
+        }else{
+            userInfoDTO.setId(1L);
+            userInfoDTO.setName("模拟用户");
+            userInfoDTO.setUserType(1L);
+        }
+
+        return userInfoDTO;
+    }
+}
---
+++ src/main/java/com/team07/online_shopping_mall/model/dto/UserInfoDTO.java
@@ -0,0 +1,10 @@
+package com.team07.online_shopping_mall.model.dto;
+
+import lombok.Data;
+
+@Data
+public class UserInfoDTO {
+    private Long id;
+    private String name;
+    private Long userType;
+}
