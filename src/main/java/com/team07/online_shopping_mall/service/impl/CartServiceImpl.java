29a80367797226a77401c67ce1b93b2287ab40b3
Stmyar <1050423479@qq.com>
2022-02-27 17:55:53 +0800

    购物车功能更新02生成订单

--- src/main/java/com/team07/online_shopping_mall/service/impl/CartServiceImpl.java
+++ src/main/java/com/team07/online_shopping_mall/service/impl/CartServiceImpl.java
@@ -1,11 +1,21 @@
 package com.team07.online_shopping_mall.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.team07.online_shopping_mall.mapper.OrderItemMapper;
+import com.team07.online_shopping_mall.mapper.OrderMapper;
+import com.team07.online_shopping_mall.mapper.ProductMapper;
 import com.team07.online_shopping_mall.model.domain.Cart;
 import com.team07.online_shopping_mall.mapper.CartMapper;
+import com.team07.online_shopping_mall.model.domain.Order;
+import com.team07.online_shopping_mall.model.domain.OrderItem;
+import com.team07.online_shopping_mall.model.domain.Product;
+import com.team07.online_shopping_mall.model.dto.OrderInfoDTO;
 import com.team07.online_shopping_mall.service.CartService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
 
 /**
  * <p>
@@ -21,10 +31,19 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements Ca
     @Autowired
     CartMapper cartMapper;
 
+    @Autowired
+    ProductMapper productMapper;
+
+    @Autowired
+    OrderMapper orderMapper;
+
+    @Autowired
+    OrderItemMapper orderItemMapper;
+
     @Override
     public boolean addCartProduct(Cart cart){
         Cart oldCart = cartMapper.selectById(cart.getId());
-        if(!(oldCart.getQuantity() == cart.getQuantity() && oldCart.getProductId() == cart.getProductId())){
+        if(!(oldCart.getQuantity().equals(cart.getQuantity()) && oldCart.getProductId().equals(cart.getProductId()))){
             return false;
         }
         cart.setQuantity(cart.getQuantity() + 1);
@@ -34,7 +53,7 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements Ca
     @Override
     public boolean subCartProduct(Cart cart){
         Cart oldCart = cartMapper.selectById(cart.getId());
-        if(!(oldCart.getQuantity() == cart.getQuantity() && oldCart.getProductId() == cart.getProductId())){
+        if(!(oldCart.getQuantity().equals(cart.getQuantity()) && oldCart.getProductId().equals(cart.getProductId()))){
             return false;
         }
         if(cart.getQuantity() - 1 == 0){
@@ -49,6 +68,75 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements Ca
     @Override
     public boolean delCartProduct(Cart cart){
         return cartMapper.deleteById(cart.getId()) == 1;
+    }
+
+    @Override
+    public Map<Long, List<OrderItem>> classifyByShop(List<Cart> cartList){
+        Map<Long, List<OrderItem>> orderItemMap = new TreeMap<>();
+        // 遍历cartList，按店铺分类生成初始订单项
+        for(Cart cart: cartList){
+            Product product = productMapper.selectById(cart.getProductId());
+            Long shopId = product.getShopId();
+            Long productId = product.getId();
+            String productName = product.getName();
+            String productImg = product.getImage();
+            Integer unitPrice = product.getPrice();
+            Integer quantity = cart.getQuantity();
+            OrderItem orderItem = new OrderItem().setOrderId(-1L)
+                    .setProductId(productId)
+                    .setProductName(productName)
+                    .setProductImg(productImg)
+                    .setUnitPrice(unitPrice)
+                    .setQuantity(quantity)
+                    .setTotalPrice(unitPrice * quantity);
+            // 写入map
+            List<OrderItem> orderItemList;
+            if(orderItemMap.containsKey(shopId)){
+                orderItemList = orderItemMap.get(shopId);
+            }
+            else{
+                orderItemList = new ArrayList<>();
+            }
+            orderItemList.add(orderItem);
+            orderItemMap.put(shopId,orderItemList);
+        }
+        return orderItemMap;
+    }
+
+    @Override
+    public void createOrder(Map<Long, List<OrderItem>> orderItemMap, OrderInfoDTO orderInfoDTO){
+        // 提取数据
+        String receiverAddress = orderInfoDTO.getReceiverAddress();
+        String receiverName = orderInfoDTO.getReceiverName();
+        String receiverMobile = orderInfoDTO.getReceiverMobile();
+        Integer postage = orderInfoDTO.getPostage();
+
+        Set<Long> set = orderItemMap.keySet();
+        // 遍历每个店铺的订单项集合
+        for(Long key : set){
+            List<OrderItem> thisOrderItemList = orderItemMap.get(key);
+            Long userId = orderInfoDTO.getUserId();
+            Order thisOrder = new Order().
+                    setUserId(userId)
+                    .setShopId(key)
+                    .setReceiverAddress(receiverAddress)
+                    .setReceiverName(receiverName)
+                    .setReceiverMobile(receiverMobile)
+                    .setPostage(postage);
+            // 插入新order
+            orderMapper.insert(thisOrder);
+            QueryWrapper<Order> wrapper = new QueryWrapper<>();
+            wrapper.lambda().eq(Order::getUserId,userId).eq(Order::getTotalPrice,0);
+            Order newOrder = orderMapper.selectOne(wrapper);
+            Long orderId = newOrder.getId();
+            // 遍历订单项，插入新订单项并更新订单总价
+            for(OrderItem orderItem : thisOrderItemList){
+                orderItem.setOrderId(orderId);
+                newOrder.setTotalPrice(newOrder.getTotalPrice() + orderItem.getTotalPrice()); // 总价
+                orderItemMapper.insert(orderItem);
+            }
+            orderMapper.updateById(newOrder);
+        }
     }
 }
 
