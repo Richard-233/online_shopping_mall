c9262374050165e59f98884d13b50263b2f805e2
Richard Young <565435857@qq.com>
2022-03-02 13:20:27 +0800

    目录 店铺 首页 商品列表的显示 以及可以支持价格排序 目录和用户中部分小细节的修改

--- src/main/java/com/team07/online_shopping_mall/mapper/ProductMapper.java
+++ src/main/java/com/team07/online_shopping_mall/mapper/ProductMapper.java
@@ -3,9 +3,11 @@ package com.team07.online_shopping_mall.mapper;
 import com.team07.online_shopping_mall.model.domain.Product;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.team07.online_shopping_mall.model.domain.User;
+import com.team07.online_shopping_mall.model.query.ProductListQuery;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -22,10 +24,18 @@ public interface ProductMapper extends BaseMapper<Product> {
 
     List<Product> getByName(String name);
 
-    //List<Long> identifyUser(@Param("user") User user,@Param("product") Product product);
-
-
     Long getShopIdByUserId(Long userId);
 
     List<Product> getByNameSuper(String name);
+
+    int batchUpdateSellStatus(@Param("ids") Integer[] ids, @Param("sellStatus") Integer sellStatus);
+
+    List<Product> selectListForAdmin();
+
+    List<Product> selectListForSeller(@Param("userId") Long currentUserId);
+
+    Product selectByPrimaryKey(Long id);
+
+    List<Product> selectList(@Param("query") ProductListQuery query);
+
 }
